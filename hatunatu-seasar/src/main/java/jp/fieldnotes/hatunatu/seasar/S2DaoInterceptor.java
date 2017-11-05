1ea3676bfe88f4604ef8bf3616c99bd515c1cba2
Hiroyuki Onaka <hiroyuki@fieldnotes.jp>
2017-11-05 20:22:37 +0900

    Backout hatunatu-seasar package.

--- hatunatu-seasar/src/main/java/jp/fieldnotes/hatunatu/seasar/S2DaoInterceptor.java
+++ hatunatu-seasar/src/main/java/jp/fieldnotes/hatunatu/seasar/S2DaoInterceptor.java
@@ -13,7 +13,7 @@
  * either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
-package jp.fieldnotes.hatunatu.lastadi.interceptors;
+package jp.fieldnotes.hatunatu.seasar;
 
 import jp.fieldnotes.hatunatu.api.DaoMetaData;
 import jp.fieldnotes.hatunatu.api.DaoMetaDataFactory;
